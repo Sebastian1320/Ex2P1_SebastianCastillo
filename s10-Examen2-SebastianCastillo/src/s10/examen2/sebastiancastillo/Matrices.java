19b14d5ca4088a6929b319f0a8847b80fa257067
Sebastian1320 <147879028+Sebastian1320@users.noreply.github.com>
2023-12-15 19:23:42 -0600

    Add files via upload

---
+++ s10-Examen2-SebastianCastillo/src/s10/examen2/sebastiancastillo/Matrices.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package s10.examen2.sebastiancastillo;
+import java.util.Random;
+/**
+ *
+ * @author Asus
+ */
+public class Matrices {
+    private int matriz[][];
+    public Matrices(int matriz[][]){
+        this.matriz=matriz;
+    }
+    
+}
